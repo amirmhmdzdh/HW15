ed7ae0a011881ccdce3d7114ec91103655c890bc
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-03-18 10:40:32 +0330

    Add FractionTest

---
+++ src/test/java/ir/hw15/FractionTest.java
@@ -0,0 +1,28 @@
+package ir.hw15;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FractionTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void sub() {
+    }
+
+    @Test
+    void mul() {
+    }
+
+    @Test
+    void div() {
+    }
+
+    @Test
+    void toFloatingPoint() {
+    }
+}
