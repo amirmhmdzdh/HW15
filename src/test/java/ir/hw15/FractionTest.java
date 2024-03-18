96b2f0c019d7e892129c74ea370e1b2f2086a6ba
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-03-18 11:02:03 +0330

    Add addTest

--- src/test/java/ir/hw15/FractionTest.java
+++ src/test/java/ir/hw15/FractionTest.java
@@ -8,10 +8,20 @@ class FractionTest {
 
     @Test
     void add() {
+        Fraction fraction1 = new Fraction(2, 3);
+        Fraction fraction2 = new Fraction(3, 4);
+        Fraction expected = new Fraction(17, 12);
+
+        Fraction sum = fraction1.add(fraction2);
+
+        assertEquals(expected.getNumerator(), sum.getNumerator());
+        assertEquals(expected.getDenominator(), sum.getDenominator());
+
     }
 
     @Test
     void sub() {
+
     }
 
     @Test
