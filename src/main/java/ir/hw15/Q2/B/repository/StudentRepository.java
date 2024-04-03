752488a06c54e94f837a600e055cb265f26961a3
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-04-03 18:51:40 +0430

    add Method

--- src/main/java/ir/hw15/Q2/B/repository/StudentRepository.java
+++ src/main/java/ir/hw15/Q2/B/repository/StudentRepository.java
@@ -1,4 +1,88 @@
 package ir.hw15.Q2.B.repository;
 
+import ir.hw15.Q2.B.model.Student;
+import ir.hw15.Q2.B.model.Teacher;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
 public class StudentRepository {
+    private SessionFactory sessionFactory;
+
+    public StudentRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void save(Student student) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(student);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void update(Student student) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.update(student);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void delete(int id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            Student student = session.get(Student.class, id);
+            if (student != null) {
+                session.delete(student);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void loadAll() {
+        Session session = sessionFactory.openSession();
+        List<Student> students = session.createQuery("FROM Student ").list();
+        session.close();
+        System.out.println(students);
+    }
+
+    public boolean contains(int id) {
+        Session session = sessionFactory.openSession();
+        try {
+            return session.contains(session.get(Student.class, id));
+        } finally {
+            session.close();
+
+        }
+    }
 }
--- src/main/java/ir/hw15/Q2/B/repository/TeacherRepository.java
+++ src/main/java/ir/hw15/Q2/B/repository/TeacherRepository.java
@@ -1,4 +1,88 @@
 package ir.hw15.Q2.B.repository;
 
+import ir.hw15.Q2.A.Person;
+import ir.hw15.Q2.B.model.Teacher;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
 public class TeacherRepository {
+    private SessionFactory sessionFactory;
+
+    public TeacherRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void save(Teacher teacher) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.save(teacher);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void update(Teacher teacher) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            session.update(teacher);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void delete(int id) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            Teacher teacher = session.get(Teacher.class, id);
+            if (teacher != null) {
+                session.delete(teacher);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public void loadAll() {
+        Session session = sessionFactory.openSession();
+        List<Teacher> teachers = session.createQuery("FROM Teacher ").list();
+        session.close();
+        System.out.println(teachers);
+    }
+
+    public boolean contains(int id) {
+        Session session = sessionFactory.openSession();
+        try {
+            return session.contains(session.get(Teacher.class, id));
+        } finally {
+            session.close();
+
+        }
+    }
 }
